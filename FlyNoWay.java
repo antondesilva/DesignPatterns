1af45233fdd2f24e1fe83b0fc987d4b2c78d9a31
Anton Desilva <antondesilva09@yahoo.com>
2015-07-18 14:25:53 -0400

    Updated with additional functionality

---
+++ FlyNoWay.java
@@ -0,0 +1,10 @@
+package design.patterns;
+
+public class FlyNoWay implements FlyBehavior {
+
+	@Override
+	public void fly() {
+		System.out.println("<<I can't fly !>>");
+	}
+
+}
---
+++ FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package design.patterns;
+
+public class FlyRocketPowered implements FlyBehavior{
+	public void fly()
+	{
+		System.out.println("I'm flying with a rocket !");
+	}
+}
